efb192c197bbc8aa0380fecdd9e4f9f65e465c9d
Camille Condrey <camillecondrey@gmail.com>
2017-07-16 21:10:32 -0600

    push

---
+++ app.js
@@ -0,0 +1,16 @@
+
+	$("#port-nav").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $('#Portfolio').offset().top},
+	        'slow');
+	});
+    $("#contact-nav").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $('#contact').offset().top},
+	        'slow');
+	});
+    $("#about-nav").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $('#about').offset().top},
+	        'slow');
+	});
